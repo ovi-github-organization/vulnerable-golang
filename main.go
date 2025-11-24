27cc32f05ac0ab355490dcf82e1be3aecc494561
ovijeet <osircar@endor.ai>
2025-11-24 17:07:47 +0530

    init add vulns

--- main.go
+++ main.go
@@ -5,8 +5,10 @@ import (
 
 	_ "github.com/dgrijalva/jwt-go"
 	_ "github.com/gogo/protobuf/proto"
+	_ "github.com/grafana/grafana-plugin-sdk-go"
 	_ "github.com/hashicorp/golang-lru"
 	_ "github.com/owncast/owncast/logging"
+	_ "github.com/xanzy/go-gitlab"
 )
 
 func main() {
